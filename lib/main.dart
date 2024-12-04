43a4737515b24571b05c3c0f8f1259284a1d135f
SepurLegend <161539066+SepurLegend@users.noreply.github.com>
2024-12-04 17:37:28 +0700

    Membuat search screen
    Search screen ini dapat mencari buku sesuai dengan data buku yang ada, jika buku tidak ada, maka tidak akan muncul apa apa

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
-import 'package:bookhub/screens/home_screen.dart';
-import 'package:flutter/material.dart';
+  import 'package:bookhub/screens/home_screen.dart'; 
+  import 'package:bookhub/screens/search_screen.dart'; 
+  import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BookHub',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(), // Panggil MainScreen dari screens/main_screen.dart
-    );
+  void main() {
+    runApp(MyApp());
   }
-}
+
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'BookHub',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(),
+        routes: {
+          SearchScreen.routeName: (context) => SearchScreen(), 
+      }, 
+      );  
+    }
+  }
